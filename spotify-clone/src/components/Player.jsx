74b04c0328aed2709a3d9d68689ca6ac325c7bcc
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-03-10 16:25:49 +0530

    improve

--- spotify-clone/src/components/Player.jsx
+++ spotify-clone/src/components/Player.jsx
@@ -24,10 +24,9 @@ const Player = () => {
             <div className='flex items-center gap-5'>
                 <p>1:06</p>
                 <div className='w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer'>
-                    <hr className='h-1 border-none w-0 bg-green-800 rounded-full'/>
+                    <hr className='h-1 border-none w-full bg-green-800 rounded-full'/>
                 </div>
                 <p>3:20</p>
-
             </div>
         </div>
     </div>
