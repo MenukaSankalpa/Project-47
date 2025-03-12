5dffff1e51966b7212e38d6baeb784ce0af1f81f
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-03-12 11:22:53 +0530

    add all album and made files

--- spotify-clone/src/components/DisplayHome.jsx
+++ spotify-clone/src/components/DisplayHome.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from './Navbar'
+import { albumsData } from '../assets/assets'
 
 const DisplayHome = () => {
   return (
     <>
     <Navbar />
+    <div className='my-5 font-bold text-2xl'>
+        {albumsData.map((item,index)=>(<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
+    </div>
     </>
   )
 }
