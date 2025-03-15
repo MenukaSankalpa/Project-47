e4d2e931e40a113d1a9005f725b239668dd488b7
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-03-15 09:05:32 +0530

    updated songs.data map

--- spotify-clone/src/components/DisplayAlbum.jsx
+++ spotify-clone/src/components/DisplayAlbum.jsx
@@ -1,7 +1,7 @@
 import React from 'react' 
 import Navbar from './Navbar'
 import { useParams } from 'react-router-dom'
-import { albumsData, assets } from '../assets/assets';
+import { albumsData, assets, songsData } from '../assets/assets';
 
 const DisplayAlbum = () => {
 
@@ -22,12 +22,27 @@ const DisplayAlbum = () => {
             <p className='mt-1'>
                 <img className='inline-block w-5' src={assets.spotify_logo} alt="" />
                 <b>Spotify</b>
-                
-
+                • 1,324,154 likes
+                •<b>50 songs,</b>
+                about 2 hr 30 min
             </p>
-
           </div>
        </div>
+       <div className='grid grid-cols-3 sm:grid-cols-4 mt-10 mb-4 pl-2 text-[#a7a7a7]'>
+        <p><b className='mr-4'>#</b>Title</p>
+        <p>Album</p>
+        <p className='hidden sm:block'>Date Added</p>
+        <img className='m-auto w-4' src={assets.clock_icon} alt="" />
+       </div>
+       <hr />
+       {
+        songsData.map((item,index)=>(
+          <div key={index} className='grid grid-cols-3 sm:grid-cols-4 gap-2 p-2 items-center text-[#a7a7a7] hover:bg-[#ffffff2b] cursor-pointer'>
+
+          </div>
+
+        ))
+       }
     </>
   )
 }
