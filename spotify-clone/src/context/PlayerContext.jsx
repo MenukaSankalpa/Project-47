ee3ee7eb318d012cd8d91c0c85b29677fd97f09d
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-03-21 11:57:22 +0530

    made song play pause function

--- spotify-clone/src/context/PlayerContext.jsx
+++ spotify-clone/src/context/PlayerContext.jsx
@@ -1,4 +1,5 @@
-import { createContext, useRef } from "react";
+import { createContext, useRef, useState } from "react";
+import { songsData } from "../assets/assets";
 
 export const PlayerContext = createContext();
 
@@ -6,11 +7,42 @@ export const PlayerContext = createContext();
 const PlayerContextProvider = (props) => {
 
     const audioRef = useRef();
+    const seekBg = useRef();
+    const seekBar = useRef();
+    
+    const [track, setTrack] = useState(songsData[0]);
+    const [playStatus, setPlayStatus] = useState(false);
+    const [time, setTime] = useState({
+        currentTime: {
+            second: 0,
+            minute: 0
+        },
+        totalTime: {
+            second: 0,
+            minute: 0
+
+        }
+    })
+
+    const play = () => {
+        audioRef.current.play();
+        setPlayStatus(true);
+    }
+
+    const pause = () => {
+        audioRef.current.pause();
+        setPlayStatus(false);
+    }
 
 
     const contextValue = {
-        audioRef
-
+        audioRef,
+        seekBar,
+        seekBg,
+        track,setTrack,
+        playStatus,setPlayStatus,
+        time,setTime,
+        play,pause
     }
     return(
         <PlayerContext.Provider value={contextValue}>
