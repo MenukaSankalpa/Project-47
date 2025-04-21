b9c5b81bac090da0b9b13b0c2ae42d761aa39cea
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-04-21 11:35:32 +0530

    add mongodb url and initializing routes songRouter added

--- spotify-full-stack/spotify-backend/server.js
+++ spotify-full-stack/spotify-backend/server.js
@@ -1,6 +1,7 @@
-import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import express from "express";
+import songRouter from "./src/routes/songRoute.js";
 
 //app config
 const app = express();
@@ -11,5 +12,6 @@ app.use(express.json());
 app.use(cors());
 
 //initializing routes
+app.use("/api/song", songRouter)
 app.get("/", (req, res) => res.send("API Working"));
 app.listen(port, () => console.log(`Server started on ${port}`));
