4a4340c307ab5b1bba147fe36779568012beabd2
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-04-23 14:52:04 +0530

    updated

--- spotify-full-stack/spotify-backend/server.js
+++ spotify-full-stack/spotify-backend/server.js
@@ -1,6 +1,7 @@
 import cors from "cors";
 import "dotenv/config";
 import express from "express";
+import { connectCloudinary } from './src/config/cloudinary.js';
 import connectDB from "./src/config/mongodb.js";
 import songRouter from "./src/routes/songRoute.js";
 
--- spotify-full-stack/spotify-backend/src/config/cloudinary.js
+++ spotify-full-stack/spotify-backend/src/config/cloudinary.js
@@ -1,4 +1,4 @@
-import { v2 as cloudinary } from 'cloudinary'
+import { v2 as cloudinary } from 'cloudinary';
 
 const connectCloudinary = async () => {
     await cloudinary.config({
@@ -7,3 +7,5 @@ const connectCloudinary = async () => {
         api_secret: process.env.CLOUDINARY_SECRET_KEY
     })
 }
+
+export { cloudinary, connectCloudinary };
