09a71e98fb2d5ae00f0b68bbb7c7fa1e2d42d99d
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-04-25 14:36:31 +0530

    songRoute.js updated

--- spotify-full-stack/spotify-backend/src/routes/songRoute.js
+++ spotify-full-stack/spotify-backend/src/routes/songRoute.js
@@ -4,9 +4,7 @@ import upload from '../middleware/multer.js';
 
 const songRouter = express.Router();
 
-songRouter.post('/add',upload.fields([{name:'image',maxCount:1}, {name:'audio',maxCount:1}]), (req, res, next) => {console.log("POST /add hit");
-    next();
-}, addSong);
+songRouter.post('/add',upload.fields([{name:'image',maxCount:1}, {name:'audio',maxCount:1}]), addSong);
 songRouter.get('/list', listSong);
 
 export default songRouter;
