e366acddebc8cd9f356b8a2d234efeca8b82cf1f
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-06-27 15:20:57 +0530

    server.js file updated

--- spotify-full-stack/spotify-backend/server.js
+++ spotify-full-stack/spotify-backend/server.js
@@ -1,13 +1,13 @@
 import cors from 'cors';
 import "dotenv/config";
 import express from 'express';
-import { connectCloudinary } from './src/config/cloudinary.js';
+import connectCloudinary from './src/config/cloudinary.js';
 import connectDB from './src/config/mongodb.js';
 import songRouter from './src/routes/songRoute.js';
 
 //app config
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 connectDB();
 connectCloudinary();
 
@@ -16,6 +16,6 @@ app.use(express.json());
 app.use(cors());
 
 //initializing routes
-app.use("/api/song", songRouter);
+app.use("/api/song", songRouter)
 app.get("/", (req, res) => res.send("API Working"));
 app.listen(port, () =>console.log(`Server started on ${port}`));
